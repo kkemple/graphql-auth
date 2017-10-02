5f26cb9b529754b21d7688353747454690a9dbff
HaNdTriX <handtrix@gmail.com>
2017-10-02 17:48:51 +0200

    Add standard linter
    I know this linter is opinionated.
    Feel free to discard this PR.
    
    ## Changes
    
    * Added Badge to Readme
    * Added lint to test script
    
    ## ⚠️ Depends on
    
    * https://github.com/kkemple/graphql-auth/pull/4

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 export class ContextError extends Error {
-  constructor(message = '`auth` property not found on context!') {
-    super(message);
-    this.message = message;
-    this.name = 'ContextError';
+  constructor (message = '`auth` property not found on context!') {
+    super(message)
+    this.message = message
+    this.name = 'ContextError'
   }
 }
 
 export class AuthorizationError extends Error {
-  constructor(message = 'Permission Denied!') {
-    super(message);
-    this.message = message;
-    this.name = 'AuthorizationError';
+  constructor (message = 'Permission Denied!') {
+    super(message)
+    this.message = message
+    this.name = 'AuthorizationError'
   }
 }
 
-function validateScope(required, provided) {
-  let hasScope = false;
+function validateScope (required, provided) {
+  let hasScope = false
 
   required.forEach(scope => {
-    provided.forEach(function(perm) {
+    provided.forEach(function (perm) {
       // user:* -> user:create, user:view:self
-      var permRe = new RegExp('^' + perm.replace('*', '.*') + '$');
-      if (permRe.exec(scope)) hasScope = true;
-    });
-  });
+      var permRe = new RegExp('^' + perm.replace('*', '.*') + '$')
+      if (permRe.exec(scope)) hasScope = true
+    })
+  })
 
-  return hasScope;
+  return hasScope
 }
 
-export default function withAuth(scope, callback) {
-  const next = callback ? callback : scope;
-  let requiredScope = callback ? scope : null;
+export default function withAuth (scope, callback) {
+  const next = callback || scope
+  let requiredScope = callback ? scope : null
 
-  return async function(_, __, context) {
-    if (!context.auth) return new ContextError();
-    if (!context.auth.isAuthenticated)
-      return new AuthorizationError('Not Authenticated!');
+  return async function (_, __, context) {
+    if (!context.auth) return new ContextError()
+    if (!context.auth.isAuthenticated) { return new AuthorizationError('Not Authenticated!') }
 
-    if (requiredScope && typeof requiredScope === 'function')
+    if (requiredScope && typeof requiredScope === 'function') {
       requiredScope = await Promise.resolve().then(() =>
-        requiredScope(_, __, context),
-      );
+        requiredScope(_, __, context)
+      )
+    }
 
     if (
       (requiredScope && requiredScope.length && !context.auth.scope) ||
       (requiredScope && requiredScope.length &&
         !validateScope(requiredScope, context.auth.scope))
     ) {
-      return new AuthorizationError();
+      return new AuthorizationError()
     }
 
-    return next(_, __, context);
-  };
+    return next(_, __, context)
+  }
 }
