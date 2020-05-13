c2f3f8e3f4962c13e7872d6f416cde9871d8f6d7
Kurt Kemple <kurtiskemple@gmail.com>
2020-05-13 13:30:33 -0400

    update types and tests

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,57 @@
-export class ContextError extends Error {
-  constructor(message = '`auth` property not found on context!') {
-    super(message);
-    this.message = message;
-    this.name = 'ContextError';
-  }
-}
+type Resolver = (parent: any, params: any, context: any, info: any) => any;
 
-export class AuthorizationError extends Error {
-  constructor(message = 'Permission Denied!') {
-    super(message);
-    this.message = message;
-    this.name = 'AuthorizationError';
-  }
-}
-
-function validateScope(required, provided) {
+function validateScope(required: string[], provided: string[]): boolean {
   let hasScope = false;
 
-  required.forEach(scope => {
-    provided.forEach(function(perm) {
+  required.forEach((scope: string) => {
+    provided.forEach(function(providedScope: string) {
       // user:* -> user:create, user:view:self
-      var permRe = new RegExp('^' + perm.replace('*', '.*') + '$');
-      if (permRe.exec(scope)) hasScope = true;
+      var regex = new RegExp('^' + providedScope.replace('*', '.*') + '$');
+      if (regex.exec(scope)) {
+        hasScope = true;
+      }
     });
   });
 
   return hasScope;
 }
 
-export default function withAuth(scope, callback) {
-  const next = callback ? callback : scope;
+export default function withAuth(scope: any, callback?: any) {
+  const next: Resolver = callback ? callback : scope;
 
-  return async function(_, __, context, info) {
-    if (!context.auth) return new ContextError();
-    if (!context.auth.isAuthenticated)
-      return new AuthorizationError('Not Authenticated!');
+  return async function(_: any, __: any, context: any, info: any) {
+    // will hold resolved scope, if any
+    let finalScope;
 
-    let requiredScope = callback ? scope : null;
-
-    if (requiredScope && typeof requiredScope === 'function') {
-      requiredScope = await Promise.resolve().then(() =>
-        requiredScope(_, __, context, info)
-      );
+    // if no auth object on context in resolver, error out
+    if (!context.auth) {
+      return new Error('`auth` property not found on context!');
     }
 
-    if (
-      (requiredScope && requiredScope.length && !context.auth.scope) ||
-      (requiredScope &&
-        requiredScope.length &&
-        !validateScope(requiredScope, context.auth.scope))
-    ) {
-      return new AuthorizationError();
+    // if user is not authenticated, error out
+    if (!context.auth.isAuthenticated) {
+      return new Error('Not authenticated!');
+    }
+
+    // determine if we need to check scopes
+    const hasScope = !!callback;
+    if (hasScope) {
+      // check if scope is a function that resolves to required scopes
+      if (typeof scope === 'function') {
+        // we wrap the function in a promise so whether scope resolver is async or not we can handle it like it is
+        finalScope = await Promise.resolve(() => scope(_, __, context, info));
+      } else {
+        finalScope = scope;
+      }
+
+      if (finalScope && finalScope.length) {
+        if (
+          !context.auth.scope ||
+          !validateScope(finalScope, context.auth.scope)
+        ) {
+          return new Error('Permission denied!');
+        }
+      }
     }
 
     return next(_, __, context, info);
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,68 +1,98 @@
 /* eslint-env jest */
-import withAuth, { ContextError, AuthorizationError } from '../src/index';
+import withAuth from '../src/index';
 
 const noop = () => true;
 
-test('returns an ContextError if auth context is missing', async () => {
-  const result = await withAuth(noop)(null, null, {});
-  expect(result).toBeInstanceOf(ContextError);
+test('returns an error if auth context is missing', async () => {
+  const result = await withAuth(noop)(null, null, {}, {});
+  expect(result).toBeInstanceOf(Error);
 });
 
-test('returns an AuthorizationError if the user is not authenticated', async () => {
-  const result = await withAuth(noop)(null, null, {
-    auth: {
-      isAuthenticated: false,
+test('returns an error if the user is not authenticated', async () => {
+  const result = await withAuth(noop)(
+    null,
+    null,
+    {
+      auth: {
+        isAuthenticated: false,
+      },
     },
-  });
-  expect(result).toBeInstanceOf(AuthorizationError);
+    {}
+  );
+  expect(result).toBeInstanceOf(Error);
 });
 
-test('returns an AuthorizationError if the user has not the right scope', async () => {
-  const result = await withAuth(['scope:user'], noop)(null, null, {
-    auth: {
-      isAuthenticated: true,
-      scope: ['scope:post'],
+test("returns an error if the user doesn't have proper scope", async () => {
+  const result = await withAuth(['scope:user'], noop)(
+    null,
+    null,
+    {
+      auth: {
+        isAuthenticated: true,
+        scope: ['scope:post'],
+      },
     },
-  });
-  expect(result).toBeInstanceOf(AuthorizationError);
+    {}
+  );
+  expect(result).toBeInstanceOf(Error);
 });
 
-test('resolves if the user is authenticated (without scope)', async () => {
-  const result = await withAuth(noop)(null, null, {
-    auth: {
-      isAuthenticated: true,
-      scope: [],
+test('resolves if the user is authenticated', async () => {
+  const result = await withAuth(noop)(
+    null,
+    null,
+    {
+      auth: {
+        isAuthenticated: true,
+        scope: [],
+      },
     },
-  });
+    {}
+  );
   expect(result).toBe(true);
 });
 
-test('resolves if the user is authenticated (with scope)', async () => {
-  const result = await withAuth(['scope:user'], noop)(null, null, {
-    auth: {
-      isAuthenticated: true,
-      scope: ['scope:user', 'scope:post'],
+test('resolves if the user has correct scope', async () => {
+  const result = await withAuth(['scope:user'], noop)(
+    null,
+    null,
+    {
+      auth: {
+        isAuthenticated: true,
+        scope: ['scope:user', 'scope:post'],
+      },
     },
-  });
+    {}
+  );
   expect(result).toBe(true);
 });
 
-test('resolves if the user is has a wildcard scope', async () => {
-  const result = await withAuth(['scope:user'], noop)(null, null, {
-    auth: {
-      isAuthenticated: true,
-      scope: ['scope:*'],
+test('resolves if the user has a wildcard scope', async () => {
+  const result = await withAuth(['scope:user'], noop)(
+    null,
+    null,
+    {
+      auth: {
+        isAuthenticated: true,
+        scope: ['scope:*'],
+      },
     },
-  });
+    {}
+  );
   expect(result).toBe(true);
 });
 
-test('resolves if the user is has a nested wildcard scope', async () => {
-  const result = await withAuth(['scope:user:delete'], noop)(null, null, {
-    auth: {
-      isAuthenticated: true,
-      scope: ['scope:user:*'],
+test('resolves if the user has a nested wildcard scope', async () => {
+  const result = await withAuth(['scope:user:delete'], noop)(
+    null,
+    null,
+    {
+      auth: {
+        isAuthenticated: true,
+        scope: ['scope:user:*'],
+      },
     },
-  });
+    {}
+  );
   expect(result).toBe(true);
 });
